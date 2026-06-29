d436eecf9035434f9f8e2a117bf4f361e7a5b511
Ian Otieno Ochieng <ochiengian383@gmail.com>
2026-06-29 08:29:46 +0300

    feat: streamline module directory creation and cleanup in CoreSetup

--- src/Setups/CoreSetup.php
+++ src/Setups/CoreSetup.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Jengo\Base\Setups;
 
+use CodeIgniter\CLI\CLI;
+
 class CoreSetup extends AbstractSetup
 {
     public static function name(): string
@@ -29,31 +31,16 @@ class CoreSetup extends AbstractSetup
 
         // Create modules ecosystem directories
         $modulesDir = ROOTPATH . 'modules';
-        $groupDirs = [
-            $modulesDir . '/Core',
-            $modulesDir . '/Financial',
-        ];
 
         if (!is_dir($modulesDir)) {
             mkdir($modulesDir, 0777, true);
-        }
-
-        foreach ($groupDirs as $dir) {
-            if (!is_dir($dir)) {
-                mkdir($dir, 0777, true);
-            }
-            // Put .gitkeep inside
-            $gitkeepPath = $dir . '/.gitkeep';
-            if (!file_exists($gitkeepPath)) {
-                file_put_contents($gitkeepPath, '');
-            }
         }
 
         // Register Modules namespace in composer.json
         $composerJsonPath = ROOTPATH . 'composer.json';
         if (file_exists($composerJsonPath)) {
             $composerJson = json_decode(file_get_contents($composerJsonPath), true);
-            
+
             if (!isset($composerJson['autoload'])) {
                 $composerJson['autoload'] = [];
             }
@@ -67,9 +54,9 @@ class CoreSetup extends AbstractSetup
                     $composerJsonPath,
                     json_encode($composerJson, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES)
                 );
-                
-                \CodeIgniter\CLI\CLI::write('  ' . \CodeIgniter\CLI\CLI::color('●', 'cyan') . ' Added Modules namespace to composer.json.', 'dark_gray');
-                
+
+                CLI::write('  ' . CLI::color('●', 'cyan') . ' Added Modules namespace to composer.json.', 'dark_gray');
+
                 $composer = $this->composer();
                 $composer->run('composer dump-autoload', ROOTPATH);
             }
